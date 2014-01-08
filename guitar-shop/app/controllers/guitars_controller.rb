1bfbd11ef274603f11ca4806512688554d4f9304
saintprog <saintprog@gmail.com>
2014-01-08 10:54:00 +0400

    It's ruby style, I think

--- guitar-shop/app/controllers/guitars_controller.rb
+++ guitar-shop/app/controllers/guitars_controller.rb
@@ -1,72 +1,38 @@
 class GuitarsController < ApplicationController
+  authorize_resource
+
   def index
-    if can? :read, Guitar
-      @guitars = Guitar.all
-    else
-      redirect_to root_url
-    end
+    @guitars = Guitar.all
   end
 
   def show
-    if can? :read, Guitar
-      @guitar = Guitar.find(params[:id])
-    else
-      redirect_to root_url
-    end
+    @guitar = Guitar.find(params[:id])
   end
 
   def edit
-    if can? :update, Guitar
-      @guitar = Guitar.find(params[:id])
-    else
-      redirect_to root_url
-    end
+    @guitar = Guitar.find(params[:id])
   end
 
   def new
-    if can? :create, Guitar
-      @guitar = Guitar.new
-    else
-      redirect_to root_url
-    end
+    @guitar = Guitar.new
   end
 
   def update
-    if can? :update, Guitar
-      @guitar = Guitar.find(params[:id])
-      if @guitar.update_attributes(params[:guitar])
-        redirect_to @guitar
-      else
-        render 'edit'
-      end
-    else
-      redirect_to root_url
-    end
+    @guitar = Guitar.find(params[:id])
+    redirect_to @guitar if @guitar.update_attributes(params[:guitar])
+    render 'edit' unless @guitar.update_attributes(params[:guitar])
   end
 
   def create
-    if can? :create, Guitar
-      @guitar = Guitar.new(params[:guitar])
-   
-      if @guitar.save
-        redirect_to @guitar
-      else
-        render 'new'
-      end
-    else
-      redirect_to root_url
-    end
+    @guitar = Guitar.new(params[:guitar])
+    redirect_to @guitar if @guitar.save
+    render 'new' unless @guitar.save
   end
 
   def destroy
-    if can? :update, Guitar
-      @guitar = Guitar.find(params[:id])
-      @guitar.destroy
-     
-      redirect_to guitars_path
-    else
-      redirect_to root_url
-    end
+    @guitar = Guitar.find(params[:id])
+    @guitar.destroy
+    redirect_to guitars_path
   end
 
 end
--- guitar-shop/app/controllers/orders_controller.rb
+++ guitar-shop/app/controllers/orders_controller.rb
@@ -1,83 +1,56 @@
 class OrdersController < ApplicationController
   before_filter :authenticate_user!
+  authorize_resource
 
   def index
-    if can? :read, Order
-      @orders = Order.all
-    else
-      redirect_to root_url
-    end
+    @orders = Order.all
   end
 
   def show
-    if can? :read, Order
-    else
-      redirect_to root_url
-    end
   end
 
   def new
-    if can? :create, Order
-      @order = Order.new(:user_id => current_user.try(:id))
-    else
-      redirect_to root_url
-    end
+    @order = Order.new(:user_id => current_user.try(:id))
   end
 
   def edit
-    if can? :update, Order
-      @order = Order.find(params[:id])
-    else
-      redirect_to root_url
-    end
+    @order = Order.find(params[:id])
   end
 
   def create
-    if can? :create, Order
-      if Guitar.find(params[:guitar_id]).count !=0
-        @order = Order.new(:user_id => params[:user_id], 
-                           :guitar_id => params[:guitar_id],
-                           :cost => params[:cost],
-                           :status => params[:status])
+    if Guitar.find(params[:guitar_id]).count !=0
+      @order = Order.new(:user_id => params[:user_id], 
+                         :guitar_id => params[:guitar_id],
+                         :cost => params[:cost],
+                         :status => params[:status])
 
-        if @order.save
-          @guitar = Guitar.find(@order.guitar_id)
-          @guitar.update_attribute('count', @guitar.count-1)
-          redirect_to guitars_path
-        else
-          render 'new'
-        end
+      if @order.save
+        @guitar = Guitar.find(@order.guitar_id)
+        @guitar.update_attribute('count', @guitar.count-1)
+        redirect_to guitars_path
       else
-        
+        render 'new'
       end
     else
-      redirect_to root_url
+      
     end
   end
 
   def update
-    if can? :update, Order
-      @order = Order.find(params[:id])
-      if @order.update_attributes(params[:order])
-        redirect_to User.find(@order.user_id)
-      else
-        render 'edit'
-      end
+    @order = Order.find(params[:id])
+    if @order.update_attributes(params[:order])
+      redirect_to User.find(@order.user_id)
     else
-      redirect_to root_url
+      render 'edit'
     end
   end
 
   def destroy
-    if can? :destroy, Order
-      @order = Order.find(params[:id])
-      @guitar = Guitar.find(@order.guitar_id)
-      @order.destroy
-      @guitar.update_attribute('count', @guitar.count+1)
-      redirect_to User.find(@order.user_id)
-    else
-      redirect_to root_url
-    end
+    @order = Order.find(params[:id])
+    @guitar = Guitar.find(@order.guitar_id)
+    @order.destroy
+    @guitar.update_attribute('count', @guitar.count+1)
+    redirect_to User.find(@order.user_id)
   end
 
 end
