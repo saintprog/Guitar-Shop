76260596e5e73717fe4bf3051b4e5b915720d256
saintprog <saintprog@gmail.com>
2014-01-15 18:06:19 +0400

    bbb

--- guitar-shop/config/initializers/devise_callbacks.rb
+++ guitar-shop/config/initializers/devise_callbacks.rb
@@ -4,9 +4,12 @@ Warden::Manager.after_authentication do |user, auth, opts|
   #   order.update_attribute('status', "waiting")
   # end
 
+  session[:user_id] = user.id
+  session[:cart] = Cart.where(user_id: user.id).last
+
   Cart.where(user_id: nil).all.each do |cart|
     cart.update_attribute('user_id', user.id)
   end
 
-  Cart.create(:user_id => user.id, :guitar_ids => [])
+  Cart.create(:user_id => user.id)
 end
